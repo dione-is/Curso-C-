4b5236c526d72c7abb0837d2913a42edb5f9b81c
dione-is <dione.bombeta@outlook.com>
2022-01-26 17:34:15 -0300

    exercicio usando metodo generic

---
+++ generics set dictionary/generics set dictionary/PrintService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace generics_set_dictionary {
+    class PrintService<T> {
+
+        private T[] _values = new T[10];
+
+        private int contador = 0;
+
+
+        public void AdicionarObject(T value) { 
+            
+            if (contador == 10 ) {
+                throw new Exception(" vetor ja esta cheio");
+            }else {
+                _values[contador] =value;
+                contador++;
+            }
+        }
+
+        public T PrimeiroValor() {
+
+            if (contador == 0) {
+
+                throw new Exception("Vetor esta vazio");
+            }
+            else {
+                return _values[0];
+            }
+        }
+
+        public void Imprimir() {
+
+            Console.Write("[");
+            for (int i = 0; i < contador -1; i++) {
+
+                Console.Write(_values[i] + ", ");
+            }
+
+            if(contador > 0) {
+
+                Console.Write(_values[contador-1]);
+            }
+            Console.WriteLine("]");
+
+        }
+    }
+}
---
+++ generics set dictionary/generics set dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using generics_set_dictionary;
+
+namespace home {
+    class Program {
+        static void Main(string[] args) {
+
+
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.WriteLine("quantos numeros ?");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtd; i++) { 
+            
+                int numero = int.Parse(Console.ReadLine());
+                printService.AdicionarObject(numero);
+            }
+
+            printService.Imprimir();
+            Console.WriteLine("Primeiro: " + printService.PrimeiroValor());
+
+        }
+    }
+}
