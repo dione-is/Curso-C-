7cebfd9feb23559fed64963f896300a912d4d143
dione-is <dione.bombeta@outlook.com>
2022-01-27 11:42:56 -0300

    utilizando Hashset e SortedSet para coleçoes

---
+++ generics set dictionary/HashSet e SortedSet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace home {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("notbook");
+            set.Add("Smarth Tv 60p");
+            set.Add("playstation 5");
+
+            foreach (string s in set) {
+
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ generics set dictionary/SortedSet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace home {
+    class Program {
+        static void Main(string[] args) {
+
+            SortedSet<int> setA = new SortedSet<int>() { 2, 4, 6, 8, 10};
+            SortedSet<int> setB = new SortedSet<int>() { 2, 4, 6, 7, 9 };
+
+            // union
+            SortedSet<int> setc = new SortedSet<int>(setA);
+            setc.UnionWith(setB);
+            Console.WriteLine("Union");
+            ImprimirSortedSet(setc);
+
+            // intersection
+            SortedSet<int> setD = new SortedSet<int>(setA);
+            setD.IntersectWith(setB);
+            Console.WriteLine();
+            Console.WriteLine("Intersection");
+            ImprimirSortedSet(setD);
+
+            SortedSet<int> setE = new SortedSet<int>(setA);
+            setE.ExceptWith(setB);
+            Console.WriteLine();
+            Console.WriteLine("Except");
+            ImprimirSortedSet(setE);
+        }
+
+        static void ImprimirSortedSet<T>(IEnumerable<T> colecao) {
+
+            Console.Write("{ ");
+            foreach (T t in colecao) {
+                Console.Write(t + ", ");
+            }
+            Console.Write(" }");
+        }
+
+    }
+}
