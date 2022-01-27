01b2b2b97a71d594ee23d9d612e028b3fcd3fc9c
dione-is <dione.bombeta@outlook.com>
2022-01-27 14:58:34 -0300

    atividade log de usuario, utilizando uma colecao para guardar os dados e hashcode e Equals para salvar em um vetor usuarios com nomes distindos, o vetor hashset para melhorar a perfomance do percorrimento

---
+++ generics set dictionary/log Usuario/Entity/RegistroAcesso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace log_Usuario.Entity {
+    class RegistroAcesso {
+        public string Nome { get; set; }
+        public DateTime Horario { get; set; }
+/*
+        public override bool Equals(object? obj) {
+            return obj is RegistroAcesso acesso &&
+                   Nome == acesso.Nome;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(Nome);
+        }*/
+    }
+}
---
+++ generics set dictionary/log Usuario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using log_Usuario.Entity;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet < RegistroAcesso > set = new HashSet<RegistroAcesso>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+
+                        string[] line = sr.ReadLine().Split(' ');
+                        string nome = line[0];
+                        DateTime horario= DateTime.Parse(line[1]);
+                        set.Add(new RegistroAcesso { Nome = nome, Horario = horario });
+                    }
+
+                    Console.WriteLine("Total de Usuarios: " + set.Count);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
